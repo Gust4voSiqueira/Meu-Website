786f8ea04e0506aee5d21ba8c3d529ce64d4fb7b
Gustavo Siqueira <gustavosiqueira082@gmail.com>
2021-08-08 12:04:13 -0300

    first commit

--- src/Sections/Landing/Landing.tsx
+++ src/Sections/Landing/Landing.tsx
@@ -1,18 +1,35 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
 import { Button } from '../../components/Button/Button'
 
 import instagram from '../../Assets/instagram-icon.png'
 import capa from '../../Assets/capa-landing.png'
 
 import './Landing.css'
+import { useEffect } from 'react'
 
 
 export function Landing() {
+
+    let textoArray = " Seja Bem-Vindo ao meu Website!"
+
+    const array = textoArray.split('')
+
+    useEffect(() => {
+
+        array.forEach((letra, i) => {
+            setTimeout(function () {
+                document.getElementById("span-animation").innerHTML += letra
+            }, 75 * i)
+        })
+    },)
+
+
     return (
         <>
             <div id="landing-container">
                 <section>
-                    <span>Olá, eu sou o <h1>Gustavo Siqueira</h1> Seja Bem-Vindo ao meu Website</span>
-                    <a  href="https://www.instagram.com/gustavo_siqueira3/"><div className='div-button'><Button image={instagram} title='Instagram' /></div></a>
+                    <span id='span-animation'>Olá, eu sou o <h1>Gustavo Siqueira</h1></span>
+                    <a href="https://www.instagram.com/gustavo_siqueira3/"><div className='div-button'><Button image={instagram} title='Instagram' /></div></a>
                 </section>
 
                 <img id='capa-image' src={capa} alt="landing-image" />
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,24 +7,28 @@ import { useState } from 'react'
 
 export function Header() {
     const [main, setMain] = useState(false)
-    const handleClick = (() => {
-
-    })
 
     return (
         <>
             <div id="header-container">
-                <h1>Portifólio</h1>
+            <h1><a href="#landing-container">Portifólio</a></h1>
 
                 <nav id='main-desktop'>
-                    <li>Sobre Mim</li>
-                    <li>Projetos</li>
-                    <li>Conhecimentos</li>
-                    <li>Contato</li>
+                    <li><a href="#sobreMim-container">Sobre Mim</a></li>
+                    <li><a href="#projects-title">Projetos</a></li>
+                    <li><a href="#conhecimentos-container">Conhecimentos</a></li>
+                    <li><a href="#contact-container">Contato</a></li>
                 </nav>
-                <img id='main-icon' src={menu} alt="" onClick={() => {
-                    setMain(!main)
-                }} />
+                {main ? (
+                    <img id='main-icon' src={close} alt="" onClick={() => {
+                        setMain(!main)
+                    }} />
+                ) : (
+
+                    <img id='main-icon' src={menu} alt="" onClick={() => {
+                        setMain(!main)
+                    }} />
+                )}
 
             </div>
 
