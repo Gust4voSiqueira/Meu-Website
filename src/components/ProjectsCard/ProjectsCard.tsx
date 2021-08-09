5dabf9f14165c817de0b493772e515bd3a5800f8
Gustavo Siqueira <gustavosiqueira082@gmail.com>
2021-08-09 17:32:07 -0300

    Add files via upload

--- src/components/ProjectsCard/ProjectsCard.tsx
+++ src/components/ProjectsCard/ProjectsCard.tsx
@@ -7,15 +7,17 @@ interface ProjectsCardProps {
     title: string;
     description: string;
     tecnologies: string[];
+    website: boolean;
+    link: string;
 }
 
-export function ProjectsCard({ title, description, tecnologies }: ProjectsCardProps) {
+export function ProjectsCard({ title, description, tecnologies, website, link }: ProjectsCardProps) {
     return (
         <div id="projectsCard-container">
             <h1>{title}</h1>
             <p className='text-projects'>{description}</p>
-            
-            <div id='div-button'><Button image={LinkIcon} title='Website' /></div>
+
+            <div id='div-button'><a href={link}><Button image={LinkIcon} title={website ? "Website" : "Github"} /></a></div>
 
             {tecnologies.map((tecnologies) => {
                 return <span className='technologies'>{tecnologies}</span>
