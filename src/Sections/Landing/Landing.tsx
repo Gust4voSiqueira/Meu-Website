50e3a0e0c71a55caef6d093c485d52e398198ab9
Gustavo Siqueira <gustavosiqueira082@gmail.com>
2021-08-09 17:30:36 -0300

    Add files via upload

--- src/Sections/Landing/Landing.tsx
+++ src/Sections/Landing/Landing.tsx
@@ -29,7 +29,7 @@ export function Landing() {
             <div id="landing-container">
                 <section>
                     <span id='span-animation'>Olá, eu sou o <h1>Gustavo Siqueira</h1></span>
-                    <a href="https://www.instagram.com/gustavo_siqueira3/"><div className='div-button'><Button image={instagram} title='Instagram' /></div></a>
+                    <a className='a-button' href="https://www.instagram.com/gustavo_siqueira3/"><Button image={instagram} title='Instagram' /></a>
                 </section>
 
                 <img id='capa-image' src={capa} alt="landing-image" />
