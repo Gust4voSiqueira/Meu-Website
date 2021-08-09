3d62380c7e316669cd1a5b61c1db74a29f26e246
Gustavo Siqueira <gustavosiqueira082@gmail.com>
2021-08-09 17:31:02 -0300

    Add files via upload

--- src/Sections/Projects/Projects.tsx
+++ src/Sections/Projects/Projects.tsx
@@ -5,17 +5,23 @@ const databaseProducts = [
     {
         title: 'Burger Lets',
         description: 'O Burger Lets é um projeto desenvolvido para uma hamburgueria da minha cidade.',
-        tecnologies: ['ReactJs', 'SpringBoot']
+        tecnologies: ['ReactJs', 'SpringBoot'],
+        website: true,
+        link: 'https://github.com/Gust4voSiqueira'
     },
     {
         title: 'Igreja IPCC',
         description: 'O projeto Igreja IPCC é um website desenvolvido para a Igreja que eu frequento.',
-        tecnologies: ['ReactJs', 'SpringBoot']
+        tecnologies: ['ReactJs', 'SpringBoot'],
+        website: false,
+        link: 'https://github.com/Gust4voSiqueira/Igreja-IPCC'
     },
     {
         title: 'Tasks',
         description: 'Tasks é um gerenciador de tarefas simples.',
-        tecnologies: ['ReactJs']
+        tecnologies: ['ReactJs'],
+        website: false,
+        link: 'https://github.com/Gust4voSiqueira/Tasks'
     },
 
 ]
@@ -23,17 +29,19 @@ const databaseProducts = [
 export function Projects() {
     return (
         <>
-        <h1 id='projects-title'>Projetos</h1>
-        <div id="projects-container">
-            
-            {databaseProducts.map((databaseProducts) => {
-                return <ProjectsCard
-                    title={databaseProducts.title}
-                    description={databaseProducts.description}
-                    tecnologies={databaseProducts.tecnologies}
-                />
-            })}
-        </div>
+            <h1 id='projects-title'>Projetos</h1>
+            <div id="projects-container">
+
+                {databaseProducts.map((databaseProducts) => {
+                    return <ProjectsCard
+                        title={databaseProducts.title}
+                        description={databaseProducts.description}
+                        tecnologies={databaseProducts.tecnologies}
+                        website={databaseProducts.website}
+                        link={databaseProducts.link}
+                    />
+                })}
+            </div>
         </>
     )
 }
