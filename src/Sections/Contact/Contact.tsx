fe63328afdd40ddab893839151200a7aeaa638f6
Gustavo Siqueira <gustavosiqueira082@gmail.com>
2021-12-01 17:51:28 -0300

    Update Contact.tsx

--- src/Sections/Contact/Contact.tsx
+++ src/Sections/Contact/Contact.tsx
@@ -17,7 +17,7 @@ export function Contact() {
             <div id='contact-cards'>
                 <a href="https://www.linkedin.com/in/gustavo-siqueira00/"><Button image={LinkedinIcon} title='Linkedin' /></a>
                 <a href="https://github.com/Gust4voSiqueira"><Button image={GithubIcon} title='Github' /></a>
-                <a href="https://www.instagram.com/gustavo_siqueira3/"><Button image={InstagramIcon} title='Instagram' /></a>
+                <a href="https://www.instagram.com/gustavo.siqueira8/"><Button image={InstagramIcon} title='Instagram' /></a>
                 <a href="https://api.whatsapp.com/send?1=pt_BR&phone=+556182687061"><Button image={WhatsappIcon} title='Whatsapp' /></a>
             </div>
         </div>
